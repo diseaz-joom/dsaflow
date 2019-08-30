f7c5d7d6700a2c659ee03f4c1ae4fbd3898e5adc
Dmitry Azhichakov <diseaz@joom.com>
2019-08-30 10:37:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tools for my personal workflow.
+
+It's not intended to be universal but suitable for me personally. Use at your own risk.
+
+WARNING: it's not even pre-alpha. Absolutely experimental and unstable.
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diseaz-dsaflow",
+    version="0.0.2",
+    author="Dmitry Azhichakov",
+    author_email="dazhichakov@gmail.com",
+    description="Tools for my personal workflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/diseaz/dsaflow",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "diseaz-dsapy @ git+https://github.com/diseaz/dsapy.git",
+    ],
+)
