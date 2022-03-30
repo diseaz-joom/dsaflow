a69f233d391a5833470c90e452c5ecc52bbbbdac
Dmitry Azhichakov <diseaz@joom.com>
2022-03-30 09:21:12 +0300

    Get rid of setup.py

--- mypypath.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- mode: python; coding: utf-8 -*-
-
-import os
-import sys
-
-home = os.path.expanduser('~')
-paths = [p for p in sys.path if p.startswith(home) and 'site-packages' not in p]
-print('export MYPYPATH={}'.format(':'.join(paths)))
--- setup.py
+++ setup.py
@@ -1,34 +1,9 @@
 #!/usr/bin/python3
 
+# Workaround as described here: https://github.com/pypa/pip/issues/7953
+import site
+site.ENABLE_USER_SITE = True
+
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="diseaz-dsaflow",
-    version="0.0.3",
-    author="Dmitry Azhichakov",
-    author_email="dazhichakov@gmail.com",
-    description="Tools for my personal workflow",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/diseaz/dsaflow",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "diseaz-dsapy @ git+https://github.com/diseaz/dsapy.git",
-        "requests",
-    ],
-    entry_points={
-        "console_scripts": [
-            "jf=jf.cmd.jf:run",
-            "jflow=jflow.scripts.jflow:run",
-        ],
-    },
-)
+setuptools.setup()
